8a4aaef7577842131b3b8bd52d0b75c90844ade6
Marlus-Sabino <marlussabino@gmail.com>
2022-05-09 19:33:43 +0100

    Add connection to the API

--- app/src/main/java/me/dio/bankline_android/ui/statement/BankStatementActivity.kt
+++ app/src/main/java/me/dio/bankline_android/ui/statement/BankStatementActivity.kt
@@ -2,10 +2,16 @@ package me.dio.bankline_android.ui.statement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import me.dio.bankline_android.R
+import me.dio.bankline_android.data.remote.State
 import me.dio.bankline_android.databinding.ActivityBankStatementBinding
 import me.dio.bankline_android.databinding.ActivityWelcomeBinding
 import me.dio.bankline_android.domain.Correntista
+import me.dio.bankline_android.domain.Movimentacao
+import me.dio.bankline_android.domain.TipoMovimentacao
 
 class BankStatementActivity : AppCompatActivity() {
 
@@ -21,8 +27,30 @@ class BankStatementActivity : AppCompatActivity() {
         intent.getParcelableExtra<Correntista>(EXTRA_ACCOUNT_HOLDER) ?: throw IllegalArgumentException()
     }
 
+    private val viewModel by viewModels<BankStatementViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.rvBankStatement.layoutManager = LinearLayoutManager(this)
+
+        findBankStatement()
+    }
+
+    private fun findBankStatement() {
+        viewModel.findBankStatement(accountHolder.id).observe(this){ state->
+            when(state){
+                is State.Success -> {
+                    binding.rvBankStatement.adapter = state.data?.let { BankStatementAdapter(it) }
+                    binding.srlBankStatement.isRefreshing = false
+                }
+                is State.Error -> {
+                    state.message?.let { Snackbar.make(binding.rvBankStatement, it, Snackbar.LENGTH_LONG).show() }
+                    binding.srlBankStatement.isRefreshing = false
+                }
+                State.Wait -> binding.srlBankStatement.isRefreshing = true
+            }
+        }
     }
 }
